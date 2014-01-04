ba1b00b40b7731a8a706306126c03cb366c437b4
Karthik Jagadeesh <karthikjagadeesh@gmail.com>
2014-01-03 17:28:44 -0800

    cleaning up the yearSold feature

--- Scripts/zillowdata.py
+++ Scripts/zillowdata.py
@@ -1,17 +1,19 @@
 import xml.etree.ElementTree as ET
 import urllib
+import datetime
 
 def get_house_value(address, csz):
 	#This method returns a tuple (currValue, soldValue)
 	#Format of address = "14232 Shady Oak Ct"
 	#Format of csz = "Saratoga, CA"
 	if(address == '' or csz == ''):
-		return address, csz, ''
+		return address, csz, 0
 	params = urllib.urlencode({'zws-id':'X1-ZWz1bbf8u6dqff_a1050', 'address':address, 'citystatezip':csz})
 	url = "http://www.zillow.com/webservice/GetDeepSearchResults.htm?%s"
 	zillow_xml = urllib.urlopen(url%params).read()
 	root = ET.fromstring(zillow_xml)
 	currValue,soldValue,soldDate = '','',''
+	years = 0
 	for child in root.iter('amount'):
 		if(child.text):
 			currValue = child.text
@@ -21,22 +23,38 @@ def get_house_value(address, csz):
 	for child in root.iter('lastSoldDate'):
 		if(child.text):
 			soldDate = child.text
-	return currValue, soldValue, soldDate
+	if soldValue and not currValue:
+		currValue = soldValue
+	elif currValue and not soldValue:
+		soldValue = currValue
+	if soldDate:
+		timeSincePurchase = datetime.datetime.today() - datetime.datetime.strptime(soldDate, '%m/%d/%Y')
+		years = timeSincePurchase.days/365
+	return currValue, soldValue, years
 
-f = open('Data/donor_data.csv', 'r')
-output = open('Data/donor_features.csv', 'w')
-donations = f.readlines()[0].split('\r')
-donations.pop(0)
-output.write("Name, Address, City, State, Donation Amount, Current House Price, Price House was Sold At, Date House was Sold, Data Set \n")
-for line in donations:
-	elements = line.strip().split(',')
-	name = elements[1]
-	addr = elements[8]
-	amount = elements[2]
-	csz = elements[10] + ", " + elements[11]
-	currVal,soldVal,soldDate = get_house_value(addr, csz)
-	out = name + "," + addr + "," + csz + "," + amount + ","
-	out += currVal + "," + soldVal + "," + soldDate + ",1" + "\n"
-	output.write(out)
-f.close()
+filenames = ['Data/donor_data.csv', 'Data/donor_data2.csv']
+user_id = 0
+
+for filename in filenames:
+	f = open(filename, 'r')
+	output = open('Data/donor_features2.csv', 'w')
+	donations = f.readlines()[0].split('\r')
+	donations.pop(0)
+	output.write("id, city, state, donation, house_price, house_sold_at, years_sold\n")
+	for line in donations:
+		elements = line.strip().split(',')
+		name = elements[1]
+		addr = elements[8]
+		amount = elements[2]
+		city = elements[10]
+		state = elements[11]
+		csz = "%s,%s"%(city, state)
+		if not addr or not city or not state:
+			continue
+		currVal,soldVal,years = get_house_value(addr, csz)
+		print (user_id, city, state, amount, currVal, soldVal, years)
+		out = "%d, %s, %s, %s, %s, %s, %d\n"%(user_id, city, state, amount, currVal, soldVal, years)
+		output.write(out)
+		user_id = user_id + 1
+	f.close()
 output.close()
