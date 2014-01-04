acaf8bb41e2833f2581c8ebfbab1350437ff69d6
rahulmohan <rooldacool@gmail.com>
2014-01-03 22:09:25 -0800

    Added cross validation to test robustness of model

--- Scripts/predictDonation.py
+++ Scripts/predictDonation.py
@@ -1,48 +1,53 @@
 import numpy as np
 import scipy
-import csv
+from sklearn.linear_model import LogisticRegression
 from sklearn import svm
+from sklearn import cross_validation
 
 donorFeatures = np.loadtxt("modelData1.txt");
+class1 = 0;
+class2 = 0;
+class3 = 0;
 
 for i in range(len(donorFeatures[:,3])):
 
 	# Group donation amounts into categories
-	if ( donorFeatures[i,3] < 90):
+
+	if ( donorFeatures[i,3] < 60):
 		donorFeatures[i,3] = 1;
-	elif ( donorFeatures[i,3] >= 90 and donorFeatures[i,3] < 250):
+		class1 = class1 + 1;
+	elif ( donorFeatures[i,3] >= 60 and donorFeatures[i,3] < 200):
 		donorFeatures[i,3] = 2;
-	elif ( donorFeatures[i,3] >= 250):
+		class2 = class2 + 1;
+	elif ( donorFeatures[i,3] >= 200):
 		donorFeatures[i,3] = 3;
+		class3 = class3 + 1;
 
-svmModel = svm.SVC(C=10, gamma=1);
+print "Number of Class 1 Samples: " + str(class1);
+print "Number of Class 2 Samples: " + str(class2);
+print "Number of Class 3 Samples: " + str(class3);
 
-model = svmModel.fit(donorFeatures[:,0:2], donorFeatures[:,3]);
-predLabels = model.predict(donorFeatures[:,0:2]);
-labelMatch = np.zeros((len(predLabels), 1));
+# Classes are imbalanced right now - most people are in class 1
 
-for i in range(len(predLabels)):
-	if (predLabels[i] == donorFeatures[i,3]):
-		labelMatch[i] = 1;
-	else:
-		labelMatch[i] = 0;
+meanData0 = np.mean(donorFeatures[:,0]);
+stdData0 = np.std(donorFeatures[:,0]);
 
-sumLabelMatch = sum(labelMatch);
-predictionAccuracy = sumLabelMatch/len(labelMatch);
+meanData1 = np.mean(donorFeatures[:,1]);
+stdData1 = np.std(donorFeatures[:,1])
 
-print "Training Set Prediction Accuracy (SVM): " + str(predictionAccuracy * 100) + "%";
+meanData2 = np.mean(donorFeatures[:,2]);
+stdData2 = np.std(donorFeatures[:,2])
 
+# Standardize Data
 
+donorFeatures[:,0] = (donorFeatures[:,0] - meanData0) / stdData0;
+donorFeatures[:,1] = (donorFeatures[:,1] - meanData1) / stdData1;
+donorFeatures[:,2] = (donorFeatures[:,2] - meanData2) / stdData2;
 
+# Fit SVM and peform cross validation
 
+model = svm.SVC(kernel='rbf', C=100, gamma=2.1);
+scores = cross_validation.cross_val_score(model, donorFeatures[:,0:2], donorFeatures[:,3], cv=10);
 
-
-
-
-
-
-
-
-
-
-
+print("10 Fold Cross Validation Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2));
+print scores
